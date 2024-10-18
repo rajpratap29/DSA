7f7e2d60403cd0ee6bac095f81dda0c4f7438a43
rajpratapgit <therajpratapofficial@gmail.com>
2024-10-18 17:56:37 +0530

    Learned about inbuilt funtions

---
+++ 8_Functions/inbuiltFunction.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter two values to check minimum: ";
+    cin>>x>>y;
+    cout<<min(x,y)<<endl;
+    int root;
+    cout<<"Enter values to check square root: ";
+    cin>>root;
+    cout<<sqrt(root)<<endl;
+    int a,b;
+    cout<<"Enter two values to check maximum: ";
+    cin>>x>>y;
+    cout<<max(x,y)<<endl;
+
+    return 0;
+}
