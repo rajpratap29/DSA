32a7a79834c901bd9ef59d85982395f0c7f29a0f
rajpratapgit <therajpratapofficial@gmail.com>
2024-10-18 18:31:27 +0530

    created optimised pascal triangle

---
+++ 8_Functions/pascalOptimised.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter number of lines: ";
+    cin>>n;
+    for(int i = 0; i <= n; i++){
+        int current = 1;
+        for(int j = 0; j <= i; j++){
+            cout<<current<<" ";
+            current = current*(i-j)/(j+1);
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
