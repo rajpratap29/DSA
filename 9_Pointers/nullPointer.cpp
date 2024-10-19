8ea90272aae8b1c3930a9a9ad17ed5841de39a01
rajpratapgit <therajpratapofficial@gmail.com>
2024-10-19 11:28:49 +0530

    Null pointer good practice

---
+++ 9_Pointers/nullPointer.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Good practice will be of great importance in dsa
+    int *ptr = NULL; // reserved adress
+    cout<<ptr; // 0x0
+    // \0 -> null character
+
+    return 0;
+}
