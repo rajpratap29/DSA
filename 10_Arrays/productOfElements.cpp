4e505c7947e1c617b44a1128c4d10dd6e05fa179
rajpratapgit <therajpratapofficial@gmail.com>
2024-10-19 18:16:20 +0530

    Products of elements of array

---
+++ 10_Arrays/productOfElements.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+    int arr[n];
+    int sum = 1;
+    for(int i = 0; i < n; i++){
+        cout<<"Enter value of "<<i<<"th index: ";
+        cin>>arr[i];
+    }
+    for(int i = 0; i < n; i++){
+        sum = sum * arr[i];
+    }
+    cout<<"The addition of elements of array is: "<<sum;
+
+    return 0;
+}
