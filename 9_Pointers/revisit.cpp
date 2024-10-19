7ce392dfab57908e2bc073b56961b901bb1e54a6
rajpratapgit <therajpratapofficial@gmail.com>
2024-10-19 10:33:12 +0530

    Basic of pointers

---
+++ 9_Pointers/revisit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // cout<<sizeof(char);
+    // cout<<sizeof(int);
+
+    int x = 5;
+    int y = 5;
+    cout<<&x<<endl;
+    cout<<&y<<endl;
+
+    return 0;
+}
