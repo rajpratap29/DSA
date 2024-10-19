d641e11746dd915fdd5ab1dd76752fbaf3e6cfdc
rajpratapgit <therajpratapofficial@gmail.com>
2024-10-19 10:57:02 +0530

    swap to number using pointer

---
+++ 9_Pointers/passByReference.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
+
+}
+
+int main(){
+    int a = 10, b = 5;
+    swap(&a, &b);
+    cout<<"a = "<<a<<", b = "<<b;
+
+    return 0;
+}
