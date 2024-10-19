d2e8781ac802b8f1adb9d1baee83c498419dc57e
rajpratapgit <therajpratapofficial@gmail.com>
2024-10-19 11:20:43 +0530

    First and last digit of number using pointer

---
+++ 9_Pointers/firstAndLastDigit.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void find(int n, int *ptr1, int *ptr2){
+    *ptr2 = n % 10;
+    while(n > 9){
+        n/=10;
+    }
+    *ptr1 = n;
+}
+
+int main(){
+    int n;
+    cout<<"Enter a number to find its first and last digit: ";
+    cin>>n;
+    int firstDigit, lastDigit;
+    int *ptr1 = &firstDigit;
+    int *ptr2 = &lastDigit;
+    find(n, ptr1, ptr2);
+    cout<<"First digit: "<<firstDigit<<", Last Digit: "<<lastDigit;
+
+    return 0;
+}
