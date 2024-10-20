c11e1180003ff8bf44e6882547fe6a0de0196bac
rajpratap29 <therajpratapofficial@gmail.com>
2024-10-20 17:24:12 +0530

    occurence of x at last position

---
+++ 11_MoreArrays/occurenceOfX.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v;
+    v.push_back(9);
+    v.push_back(10);
+    v.push_back(6);
+    v.push_back(12);
+    v.push_back(11);
+    v.push_back(6);
+    v.push_back(0);
+    int value;
+    int found;
+    bool flag = false;
+    cout<<"Enter to value you want to check at which position last appeared: ";
+    cin>>value;
+    for(int i = 0; i < v.size(); i++){
+        if(value == v[i]){
+            found = i;
+            flag = true;
+        }
+    }
+    if(!flag){
+        cout<<"Given value is not present in an vector array.";
+    }
+    else{
+        cout<<"Value found at position: "<<found;
+    }
+
+    return 0;
+}
